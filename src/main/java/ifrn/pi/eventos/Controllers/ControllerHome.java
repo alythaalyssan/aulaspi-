f0e3f05eb0d157ba0e57bde9ed4ba2b0f92e4f67
Alytha Alyssan Oliveira de França <alyssan.alytha@academico.ifrn.edu.br>
2022-08-24 14:44:46 -0300

    Colocando os arquivos iniciais da minha aplicação

---
+++ src/main/java/ifrn/pi/eventos/Controllers/ControllerHome.java
@@ -0,0 +1,12 @@
+package ifrn.pi.eventos.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ControllerHome {
+@RequestMapping("/")
+	public String home() {
+		return "Home";
+	}
+}
